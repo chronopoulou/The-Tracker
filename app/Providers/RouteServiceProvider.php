83162998e853b94ede5eb55b237f0a62e2a93c64
Johanna <nikosmouroutis@Nikoss-MacBook-Pro.local>
2018-07-09 19:22:27 +0300

    remove admin routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,7 +39,6 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
-        $this->mapAdminRoutes();
 
         //
     }
@@ -71,22 +70,5 @@ class RouteServiceProvider extends ServiceProvider
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
-    }
-
-
-    /**
-     * Define the "admin" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapAdminRoutes()
-    {
-        Route::middleware('web')
-            ->prefix('admin')
-            ->as('admin.')
-            ->namespace($this->namespace.'\Admin')
-            ->group(base_path('routes/admin.php'));
     }
 }
